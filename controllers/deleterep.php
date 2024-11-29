0b8f344e90728f9177a75b0eb88f88857cdefd81
khadijaderbel <Derbel.khadija@esprit.tn>
2024-11-29 10:19:29 +0100

    deleterep

--- controllers/deleterep.php
+++ controllers/deleterep.php
@@ -2,9 +2,10 @@
 require_once 'database.php';
 include_once '../model/rep.php';
 
-// Fonction pour supprimer une réclamation
+// Fonction pour supprimer une réponse
 function supprimerreponse($id) {
-    $sql = "DELETE FROM reponse WHERE id = :id_rep";
+    // Use 'id_rep' instead of 'id' in the SQL query
+    $sql = "DELETE FROM reponse WHERE id_rep = :id_rep"; 
     $db = Config::getConnexion(); // Assurez-vous que cette méthode renvoie une instance PDO
     $req = $db->prepare($sql);
     $req->bindValue(':id_rep', $id);
